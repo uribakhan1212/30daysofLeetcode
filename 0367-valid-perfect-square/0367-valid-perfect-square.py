b3c525989e61c659729bbb2d83fe003860d43dad
Uriba Parveen Khan <111582983+uribakhan1212@users.noreply.github.com>
2024-03-15 17:39:13 +0500

    Time: 707 ms (5.36%), Space: 466.5 MB (6.43%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -8,9 +8,11 @@ class Solution(object):
             return True
         elif num > (2**31)-1 or num < 1:
             return False
-        length = num//2
         if num > 2**15:
             length = (num//150)
+        else:
+            length = num//2
+  
         
         for i in range(num//length, length+1):
             if i*i == num:
